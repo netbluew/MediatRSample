73a6f90e0f68e0b1bf442287641a37beccc208e0
Jiwoong Song <netbluew@gmail.com>
2020-08-18 00:16:35 +0900

    Initialize First MediatR Sample Source

---
+++ FirstMediatR/CQRS/Queries/GetAllCarsQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MediatR;
+using System.Collections;
+using System.Collections.Generic;
+using FirstMediatR.CQRS.Models;
+using System.Threading.Tasks;
+using System.Threading;
+using FirstMediatR.Infrastructure;
+
+namespace FirstMediatR.CQRS.Queries
+{
+    public class GetAllCarsQuery : IRequest<IEnumerable<Car>>
+    {
+    }
+
+    public class GetAllCarsQueryHandler : IRequestHandler<GetAllCarsQuery, IEnumerable<Car>>
+    {
+        private readonly ICarRepository repository;
+
+        public GetAllCarsQueryHandler(ICarRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<Car>> Handle(GetAllCarsQuery request, CancellationToken cancellationToken)
+        {
+            return repository.GetAllCars();
+        }
+    }
+}
